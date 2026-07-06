c7c3d0b02342b972346e5f53d64be7d25f3b062e
cheekos1 <baszgad@gmail.com>
2026-07-06 13:06:31 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const config = require('./config');
+const logger = require('./utils/logger');
+const { registerCommand } = require('./handlers/commandHandler');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageTyping,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+  ],
+});
+
+const eventFiles = [
+  require('./events/ready'),
+  require('./events/interactionCreate'),
+  require('./events/messageCreate'),
+];
+
+for (const event of eventFiles) {
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+  logger.info(`Registered event: ${event.name}`);
+}
+
+const adminCreditCommand = require('./commands/admin_credit');
+registerCommand(adminCreditCommand.name, adminCreditCommand);
+
+const panelCommand = require('./commands/panel');
+registerCommand(panelCommand.name, panelCommand);
+
+process.on('unhandledRejection', (error) => {
+  logger.error('Unhandled promise rejection', error);
+});
+
+process.on('uncaughtException', (error) => {
+  logger.error('Uncaught exception', error);
+});
+
+const port = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Crown Whisper Bot is running.');
+}).listen(port, () => {
+  logger.info(`Health server listening on port ${port}`);
+});
+
+client.login(config.token).then(() => {
+  logger.info('Bot logged in successfully');
+}).catch((error) => {
+  logger.error('Failed to login', error);
+  process.exit(1);
+});
